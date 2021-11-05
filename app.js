9a2df4fcb4009d976e4bd41b00e6d1c58661f65a
John Patrick Roldan <91767440+roldanjohn@users.noreply.github.com>
2021-11-05 11:58:08 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const contents = document.querySelectorAll('.main-pages');
+const callback = function(entries){
+  entries.forEach(entry => {
+    if(!entry.isIntersecting){
+      return;
+    }
+    let theKids = entry.target.children;
+    Array.from(theKids).forEach(x => {
+      x.classList.toggle('inactive');
+      if(x.classList.contains('banner-div')){
+        Array.from(x.children).forEach(element => {
+          element.classList.toggle('inactive');
+        })
+      }
+    })
+    observer.unobserve(entry.target);
+  });
+};
+const options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.85
+}
+const observer = new IntersectionObserver(callback, options);
+
+contents.forEach(div => {
+  observer.observe(div);
+})
